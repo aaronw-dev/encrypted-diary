34a41ad47a749f17bc26d3e863aba1aad7eb4d24
AW_Dev <riptidegamesceo@gmail.com>
2023-11-09 22:04:01 -0800

    initial commit

---
+++ diarydecode.py
@@ -0,0 +1,11 @@
+import os
+if not os.path.exists("./random.diary"):
+    print("Diary file does not exist.")
+from cryptography.fernet import Fernet
+with open("random.diary", "rb") as file:
+    message = file.read()
+key = input("Key: ")
+fernet = Fernet(key)
+decMessage = fernet.decrypt(message).decode()
+with open("random.diary", "w") as file:
+    file.write(decMessage)
---
+++ diaryencode.py
@@ -0,0 +1,11 @@
+import os
+if not os.path.exists("./random.diary"):
+    print("Diary file does not exist.")
+from cryptography.fernet import Fernet
+with open("random.diary", "r") as file:
+    message = file.read()
+key = input("Key: ")
+fernet = Fernet(key)
+encMessage = fernet.encrypt(message.encode())
+with open("random.diary", "wb") as file:
+    file.write(encMessage)
