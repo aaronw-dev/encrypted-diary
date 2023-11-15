67f64201f7c8008eec9bcb4bfa2a29a334924d46
AW_Dev <riptidegamesceo@gmail.com>
2023-11-14 22:22:03 -0800

    added file existence checking

--- diaryencode.py
+++ diaryencode.py
@@ -1,6 +1,10 @@
 import os
 if not os.path.exists("./random.diary"):
     print("Diary file does not exist.")
+    exit()
+if not os.path.exists("./key.temp"):
+    print("Key file does not exist.")
+    exit()
 from cryptography.fernet import Fernet
 with open("random.diary", "r") as file:
     message = file.read()
