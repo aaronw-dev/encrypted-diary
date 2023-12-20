ab142bb9442b619aeaca2601ed9bb60bbd85598b
AW_Dev <riptidegamesceo@gmail.com>
2023-12-19 21:23:22 -0800

    added password confirmation

--- diaryencrypt.py
+++ diaryencrypt.py
@@ -6,7 +6,15 @@ if not os.path.exists("./random.diary"):
 from cryptography.fernet import Fernet
 with open("random.diary", "r") as file:
     message = file.read()
-entered_pw = input("Password: ")
+flag = False
+while flag == False:
+    entered_pw = input("Password: ")
+    confirm_pw = input("Confirm password: ")
+    if (entered_pw == confirm_pw):
+        flag = True
+    else:
+        print("Passwords do not match. Try again.")
+
 key = base64.b64encode(f"{entered_pw:<32}".encode("utf-8"))
 fernet = Fernet(key)
 encMessage = fernet.encrypt(message.encode())
