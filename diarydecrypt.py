cd450d3003ec508f44769556590e6b47d2b98bcf
AW_Dev <riptidegamesceo@gmail.com>
2023-12-19 21:23:13 -0800

    changed wording of input

--- diarydecrypt.py
+++ diarydecrypt.py
@@ -5,7 +5,7 @@ if not os.path.exists("./random.diary"):
 from cryptography.fernet import Fernet
 with open("random.diary", "rb") as file:
     message = file.read()
-entered_pw = input("Key: ")
+entered_pw = input("Password: ")
 key = base64.b64encode(f"{entered_pw:<32}".encode("utf-8"))
 fernet = Fernet(key)
 decMessage = fernet.decrypt(message).decode()
